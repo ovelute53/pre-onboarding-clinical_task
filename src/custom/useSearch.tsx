e257224aadf5bd7bbe37437c42a9fb4cf85abcf0
ovelute53 <ovelute53@naver.com>
2023-09-06 12:07:43 +0900

    feat: search custom hook 생성

---
+++ src/custom/useSearch.tsx
@@ -0,0 +1,36 @@
+import { useState, useRef } from 'react';
+import { getFromCache, setCache } from '@/utils/cache';
+import { api, ApiResponseType } from '@/utils/api';
+
+const useSearch = () => {
+  const [query, setQuery] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<ApiResponseType[]>([]);
+  const timerIdRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleInputChange = async (newValue: string) => {
+    setQuery(newValue);
+    const cachedData = getFromCache(newValue);
+
+    if (cachedData) {
+      setSuggestions(cachedData);
+      return;
+    }
+
+    if (timerIdRef.current) {
+      clearTimeout(timerIdRef.current);
+    }
+
+    timerIdRef.current = setTimeout(async () => {
+      const data = await api(newValue);
+      const filteredData = data.filter((item: ApiResponseType) =>
+        item.sickNm.toLowerCase().includes(newValue.toLowerCase()),
+      );
+      setCache(newValue, filteredData);
+      setSuggestions(filteredData);
+    }, 500);
+  };
+
+  return { query, suggestions, handleInputChange };
+};
+
+export default useSearch;
